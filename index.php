263c79176728cdcb1d6d7fd42aedf2380f00ea13
Nelzio <nelziositoe@gmail.com>
2018-11-02 09:11:02 +0200

    Index do site

---
+++ index.php
@@ -0,0 +1,166 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    
+    <link rel="shortcut icon" href="static/img/favicon.png" type="image/x-icon"/>
+        
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="static/plugins/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="static/plugins/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="static/plugins/nelzio/nelzio.css">
+    <link rel="stylesheet" type="text/css" href="static/css/form.css"/>
+        
+    <link rel="stylesheet"type="text/css" href="static/css/estilo.css"/>
+
+    <title>Tendências tecnológicas e mentes criativas</title>
+  </head>
+  <body>
+    
+
+    <!--Navbar-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+	  <a class="navbar-brand offset-sm-4 offset-md-0" href="#"><img src="static/img/Asset 5@100x.png" height="100" alt=""></a>
+
+	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	    <ul class="navbar-nav mr-auto">
+	    </ul>
+	  </div>
+	  <p class="col-sm-12 col-md-auto">Localiacao Beira Sofala<br>Data:13 de novembro</p>
+	</nav>
+	<!--main container-->
+	<div class="main-container container-fluid">
+
+
+        <div class="mb-5 container-fluid">
+        	<!--<p class="fa fa-rotate-270 posi-absulute">2018</p>-->
+            <div class="row">
+        		
+                <div class="col-md-12">
+                    <div id="center nopading" style="height: unset;">
+                       
+                       <h1 id="to">Design.</h1>
+                       <div id="div_video">
+                       	<p class="fa fa-rotate-270" id="vertical">Develop.</p>
+                            <video id="video" class="" controls="controls" poster="static/img/Play%20Icon.png">
+                            <source src="static/videos/Video promo1_1.mp4" type="video/mp4">
+
+                            </video>
+                       </div>
+                   </div>
+                </div>
+
+
+
+
+
+            </div>
+        </div>
+
+
+        <div class="container-fluid mt-2">
+            <div class="row">
+
+                <div class="col-12 col-md-6" style="float: right !important;">
+                    <h3 id="titulo"><b>Tendências tecnológicas e mentes criativas</b> </h3>
+                    <p id="texto" class="text-justify">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+
+                </div>
+
+
+
+                <div class="col-12 col-md-6" style="float: left !important;">
+                    <h3>Workshop<br>inscreva-te</h3>
+                    <form class="nelzio-form" action="contact/index.php" method="POST" accept-charset="utf-8">
+                       <div class="form-row">
+                           <div class="col-12 form-group">
+                               <input class="form-control" type="text" id="f_name" name="name" placeholder="Nome completo" maxlength="100" size="60" name="name"/>
+                           </div>
+                           <div class="col-12 form-group">
+                               <input class="form-control" type="email" id="f_email" name="email" placeholder="E-mail" />
+                           </div>
+                           <div class="col-12 form-group">
+                               <select class="form-control" name="sexo" >
+                                   <option value="masculini">Masculino</option>
+                                   <option value="masculini">Feminino</option>
+                               </select>
+                           </div>
+                       </div>
+                       <div class="row mb-2">
+                       	<div class="col-12 col-md-6">
+                       		<input class="circles" id="check" type="checkbox" name="termos"/>&nbsp; <label for="check">Eu aceito os termos de privacidade</label>
+                       	</div>
+                       	<div class="col-12 col-md-6">
+                       		<p>Programa do workshop. 
+                       		<a href="static/img/522x5j.pdf" target="_blank">
+                       			Clique aqui. 
+                       			<i class="fa fa-file-pdf-o"></i>
+                       		</a>
+                       		</p>
+                       	</div>		
+                    	</div>
+                    	<div class="form-group">
+                           <button type="submit" class="btn btn-primary form-control">Enviar</button>
+                       </div>
+                    </form>
+
+
+                </div>
+
+            </div>
+        </div>
+
+
+        <div class="container" style="height: unset;">
+            <p id="linha"></p>
+            <div class="row justify-content-md-center">
+				    <div class="col-md-4 col-12">
+				      <h1 class="bola">Patrocinadores</h1>
+				    </div>
+            </div>
+            <div id="patrocinio" style="height: unset;">
+                <p>
+                    <a href="#"><img src="static/img/Asset%2011@100x.png"  width="100px"  alt="Movitel"/> </a>
+                    <a href="#"><img src="static/img/Asset 8@100x.png"     width="100px"  alt="Mnet"/> </a>
+                </p>
+            </div>
+        </div>
+
+        
+
+
+
+
+	</div>
+	<!--end main container-->
+
+	<!--footer-->
+	<div class="container-fluid text-center bg-dark py-3">
+        <div class="copyright">
+            <p> facebook-twiter instagram</p>
+             <p><span>&copy; MSolution™ and CEG.</span> <span>DEFY THE CURRENT™</span> All rights reserved.</p>
+        </div>
+    </div>
+	<!--end footer-->
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="static/plugins/jquery/jquery.min.js"></script>
+    <script src="static/plugins/bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
